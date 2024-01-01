b46550d05d8ae0859b1763588fe9ce20acae42c9
ranoob <whyfck@gmail.com>
2024-01-01 19:14:55 -0300

    feat: raycasting variables

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import math
+
 RES = WIDTH, HEIGHT = 1600, 900
 FPS = 60
 
@@ -5,3 +7,13 @@ PLAYER_POSITION = 1.5, 5  # mini_map
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROTATION_SPEED = 0.002
+
+# RAYCASTING
+
+# FOV is the field of view of the player
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2  # // stands for integer division
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
