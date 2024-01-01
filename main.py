6120114b8ccf3c66347a05ac8f7fb6bf9dda621b
ranoob <whyfck@gmail.com>
2024-01-01 19:14:03 -0300

    feat: import raycasting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from settings import *
 from map import *
 from player import *
+from raycasting import *
 
 
 class Game:
@@ -16,9 +17,11 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.raycasting = RayCasting(self)
 
     def update(self):
         self.player.update()
+        self.raycasting.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
