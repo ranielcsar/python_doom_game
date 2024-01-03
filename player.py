d42f95c437bf64f33b6323680574675010279065
ranoob <whyfck@gmail.com>
2024-01-03 13:56:42 -0300

    feat: mouse control

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player:
         self.game = game
         self.x, self.y = PLAYER_POSITION
         self.angle = PLAYER_ANGLE
+        self.relative_movement = 0
 
     def movement(self):
         sin_a = math.sin(self.angle)
@@ -34,21 +35,36 @@ class Player:
 
         self.check_wall_collision(delta_x, delta_y)
 
-        if keys[pg.K_LEFT]:
-            self.angle -= PLAYER_ROTATION_SPEED * self.game.delta_time
-        if keys[pg.K_RIGHT]:
-            self.angle += PLAYER_ROTATION_SPEED * self.game.delta_time
+        # if keys[pg.K_LEFT]:
+        #     self.angle -= PLAYER_ROTATION_SPEED * self.game.delta_time
+        # if keys[pg.K_RIGHT]:
+        #     self.angle += PLAYER_ROTATION_SPEED * self.game.delta_time
 
-        self.angle %= math.tau
+        self.angle %= math.tau  # 2 pi
 
     def check_wall(self, x, y):
         return (x, y) not in self.game.map.world_map
 
     def check_wall_collision(self, delta_x, delta_y):
-        if self.check_wall(int(self.x + delta_x), int(self.y)):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+
+        if self.check_wall(int(self.x + delta_x * scale), int(self.y)):
             self.x += delta_x
-        if self.check_wall(int(self.x), int(self.y + delta_y)):
+        if self.check_wall(int(self.x), int(self.y + delta_y * scale)):
             self.y += delta_y
+
+    def mouse_control(self):
+        mouse_x, mouse_y = pg.mouse.get_pos()
+
+        if mouse_x < MOUSE_BORDER_LEFT or mouse_x > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+
+        self.relative_movement = pg.mouse.get_rel()[0]
+        self.relative_movement = max(
+            -MOUSE_MAX_RELATIVE_MOVEMENT,
+            min(MOUSE_MAX_RELATIVE_MOVEMENT, self.relative_movement),
+        )
+        self.angle += self.relative_movement * MOUSE_SENSITIVITY * self.game.delta_time
 
     def draw(self):
         # pg.draw.line(
@@ -66,6 +82,7 @@ class Player:
 
     def update(self):
         self.movement()
+        self.mouse_control()
 
     @property
     def position(self):
