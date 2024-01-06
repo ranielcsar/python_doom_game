f89dbe43c0b3d5f42f8d292ed57701b41b8d5a16
ranoob <whyfck@gmail.com>
2024-01-06 11:40:02 -0300

    feat: check and recover health, game over and get damage fn

--- player.py
+++ player.py
@@ -11,6 +11,32 @@ class Player:
         self.angle = PLAYER_ANGLE
         self.relative_movement = 0
         self.shot = False
+        self.health = PLAYER_MAX_HEALTH
+        self.health_recovery_delay = 700
+        self.previous_time = pg.time.get_ticks()
+
+    def recover_health(self):
+        if self.check_health_recovery_delay() and self.health < PLAYER_MAX_HEALTH:
+            self.health += 1
+
+    def check_health_recovery_delay(self):
+        time_now = pg.time.get_ticks()
+
+        if time_now - self.previous_time > self.health_recovery_delay:
+            self.previous_time = time_now
+            return True
+
+    def check_game_over(self):
+        if self.health < 1:
+            self.game.object_renderer.game_over()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game()
+
+    def get_damage(self, damage):
+        self.health -= damage
+        self.game.object_renderer.player_damage()
+        self.game.sound.player_pain.play()
 
     def single_fire_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
@@ -88,6 +114,7 @@ class Player:
     def update(self):
         self.movement()
         self.mouse_control()
+        self.recover_health()
 
     @property
     def position(self):
