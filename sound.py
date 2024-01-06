ef9bd4eafade3858efbbc5651a862f4c10623d8d
ranoob <whyfck@gmail.com>
2024-01-05 21:35:56 -0300

    feat: npc, player and theme sounds

--- sound.py
+++ sound.py
@@ -8,3 +8,8 @@ class Sound:
         pg.mixer.init()
         self.path = "resources/sound"
         self.shotgun = pg.mixer.Sound(f"{self.path}/shotgun.wav")
+        self.npc_pain = pg.mixer.Sound(f"{self.path}/npc_pain.wav")
+        self.npc_death = pg.mixer.Sound(f"{self.path}/npc_death.wav")
+        self.npc_attack = pg.mixer.Sound(f"{self.path}/npc_attack.wav")
+        self.player_pain = pg.mixer.Sound(f"{self.path}/player_pain.wav")
+        self.theme = pg.mixer.Sound(f"{self.path}/theme.mp3")
