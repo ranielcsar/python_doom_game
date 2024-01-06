101f27cf50f368a42b3cf78a877f36f615366845
ranoob <whyfck@gmail.com>
2024-01-05 21:34:25 -0300

    feat: added global_trigger due to slow animations

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -41,13 +44,23 @@ class Game:
         self.object_renderer.draw()
         self.weapon.draw()
 
+        # 2D MODE
+        # self.screen.fill("black")
+        # self.map.draw()
+        # self.player.draw()
+
     def check_events(self):
+        self.global_trigger = False
+
         for event in pg.event.get():
             if event.type == pg.QUIT or (
                 event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE
             ):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
+
             self.player.single_fire_event(event)
 
     def run(self):
