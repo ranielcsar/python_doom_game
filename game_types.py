4d513f3e6f21239978ee3a8e61f1e7952c1f66aa
ranoob <whyfck@gmail.com>
2024-01-06 09:09:47 -0300

    feat: sprite types

--- game_types.py
+++ game_types.py
@@ -2,5 +2,8 @@ from typing import TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from main import Game
+    from sprite_object import *
 
 GameType = TypeVar("GameType", bound="Game")
+AnimatedSpriteType = TypeVar("AnimatedSpriteType", bound="AnimatedSprite")
+SpriteObjectType = TypeVar("SpriteObjectType", bound="SpriteObject")
